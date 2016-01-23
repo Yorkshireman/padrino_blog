f37690b316812ca207fd509875421c98d8fb48bb
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-23 17:34:33 +0100

    added two routes with some basic output

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,14 @@ module PadrinoBlog
 
     enable :sessions
 
+    get "/" do
+      "Hello World!"
+    end
+
+    get :about, :map => '/about_us' do
+      render :haml, "%p This is a sample blog created to demonstrate how Padrino works!"
+    end
+
     ##
     # Caching support.
     #
