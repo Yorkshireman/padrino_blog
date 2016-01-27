52d2a7bfe01e9d575e51ba041daf536feb8c0f12
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-28 00:59:51 +0100

    got User form working with User being created in the controller. Need to sort out strong params and flash notice

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -5,11 +5,18 @@ PadrinoBlog::App.controllers :users do
     render 'users/new'
   end
 
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+  post :create do
+    user = User.new(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+    user.save
+    redirect('/')
+    # redirect('/', notice: "Signed up successfully!")
   end
+
+
+  # private
+
+  # def user_params
+  #   params.require(:user).permit(:name, :password, :password_confirmation)
+  # end
 
 end
