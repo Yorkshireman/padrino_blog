7fa8290ddf37e73b94e097c45ae71a51c204bc5b
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-27 01:41:48 +0100

    test for Posts index page showing all Posts

--- spec/features/posts_features_spec.rb
+++ spec/features/posts_features_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe "Displaying Posts" do
-  it "can see posts on posts page" do
-    Post.create(title: "Title", body: "Content")
-    visit '/posts'
-    expect(page).to have_content("Title")
-    expect(page).to have_content("Content")
+  context 'Posts page' do
+    before :each do
+      Post.create(title: "Title1", body: "Content1")
+      Post.create(title: "Title2", body: "Content2")
+    end
+
+    it "can see posts on posts page" do
+      visit '/posts'
+      expect(page).to have_content("Title1")
+      expect(page).to have_content("Content1")
+      expect(page).to have_content("Title2")
+      expect(page).to have_content("Content2")
+    end
   end
 end
